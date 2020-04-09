b703e6925f743125777fabd8090a44f9f70c1df6
Normann Lou <normann.lou@chrometoaster.com>
2020-04-09 16:25:39 +1200

    Use onBeforeDelete hook to unpublish

--- src/Models/DataObjectTaxonomyTerm.php
+++ src/Models/DataObjectTaxonomyTerm.php
@@ -37,13 +37,13 @@ class DataObjectTaxonomyTerm extends DataObject
     /**
      * Make sure the linking object is unpublished from Live stage before deleting it from Draft stage.
      */
-    public function onAfterDelete()
+    public function onBeforeDelete()
     {
+        parent::onBeforeDelete();
         if ($this->hasExtension(Versioned::class)) {
             if ($this->canUnpublish()) {
                 $this->doUnpublish();
             }
         }
-        parent::onAfterDelete();
     }
 }
